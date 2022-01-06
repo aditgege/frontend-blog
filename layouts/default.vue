443b2dc7606f2bede52a5a7ab5cebb341538417e
aditgege <aditgege0@gmail.com>
2022-01-06 10:16:07 +0700

    add netlify widget

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,16 @@
 </template>
 <script>
 // import Navbar from '~/components/Navbar'
+if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+}
+
 export default {
   components: {
     // Navbar
